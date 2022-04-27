2237fdd3029d0c27e7fc5053a2a4ec0eda998124
Megan Ly <meganly@megan-ly.local>
2022-04-27 14:49:48 -0700

    First crack at json parser

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+fn main() {
+
+    use std::collections::HashMap;
+    use std::str::Chars;
+    use std::iter::Peekable;
+
+    enum Json {
+        Object(HashMap<String, Json>),
+        Array(Vec<Json>),
+        String(String),
+        Number(f64),
+        Bool(bool),
+        Null,
+    }
+
+    fn parse_json(chars: &mut Peekable<Chars>) -> Json {
+        let mut tk = chars.next().unwrap();
+        if tk == '{' {
+            let mut arg = HashMap::new();
+            while tk != '}' {
+                let mut key = String::new();
+                chars.next();
+                tk = chars.next().unwrap();
+                while tk != '"' {
+                    key.push_str(&tk.to_string());
+                    tk = chars.next().unwrap();
+                }
+                chars.next();
+                let value = parse_json(chars);
+                arg.insert(key, value);
+                tk = chars.next().unwrap();
+            }
+            return Json::Object(arg);
+        } else if tk == '[' {
+            let mut arg = Vec::new();
+            while tk != ']' {
+                arg.push(parse_json(chars));
+                tk = chars.next().unwrap();
+            }
+            return Json::Array(arg);
+        } else if tk == '"' {
+            let mut arg = String::new();
+            tk = chars.next().unwrap();
+            while tk != '"' {
+                arg.push_str(&tk.to_string());
+                tk = chars.next().unwrap();
+            }
+            return Json::String(arg);
+        } else if tk == 'n' {
+            chars.next();
+            chars.next();
+            chars.next();
+            return Json::Null;
+        } else if tk == 't' {
+            chars.next();
+            chars.next();
+            chars.next();
+            return Json::Bool(true);
+        } else if tk == 'f' {
+            chars.next();
+            chars.next();
+            chars.next();
+            chars.next();
+            return Json::Bool(false);
+        } else {
+            let mut arg = String::new();
+            while chars.peek().unwrap().is_numeric() || chars.peek().unwrap() == &'.' {
+                arg.push_str(&tk.to_string());
+                tk = chars.next().unwrap();
+            }
+            let arg = arg.parse().unwrap();
+            return Json::Number(arg);
+        }
+    }
+
+    let example = r#"{"key1":false,"key2":13}"#;
+    parse_json(&mut example.chars().peekable());
+
+}
---
+++ target/debug/deps/json_parser-5d407010a0aeb24d.d
@@ -0,0 +1,5 @@
+/Users/meganly/rust_projects/json_parser/target/debug/deps/json_parser-5d407010a0aeb24d: src/main.rs
+
+/Users/meganly/rust_projects/json_parser/target/debug/deps/json_parser-5d407010a0aeb24d.d: src/main.rs
+
+src/main.rs:
