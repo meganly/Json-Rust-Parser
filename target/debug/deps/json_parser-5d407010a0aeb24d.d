f30a83b8c6892ec98f66b24428c3c709bbdd3144
Megan Ly <meganly@megan-ly.local>
2022-04-29 14:27:47 -0700

    Add parser module

--- target/debug/deps/json_parser-5d407010a0aeb24d.d
+++ target/debug/deps/json_parser-5d407010a0aeb24d.d
@@ -1,5 +1,6 @@
-/Users/meganly/rust_projects/json_parser/target/debug/deps/json_parser-5d407010a0aeb24d: src/main.rs
+/Users/meganly/rust_projects/json_parser/target/debug/deps/json_parser-5d407010a0aeb24d: src/main.rs src/parser.rs
 
-/Users/meganly/rust_projects/json_parser/target/debug/deps/json_parser-5d407010a0aeb24d.d: src/main.rs
+/Users/meganly/rust_projects/json_parser/target/debug/deps/json_parser-5d407010a0aeb24d.d: src/main.rs src/parser.rs
 
 src/main.rs:
+src/parser.rs:
--- target/debug/json_parser.d
+++ target/debug/json_parser.d
@@ -1,1 +1,1 @@
-/Users/meganly/rust_projects/json_parser/target/debug/json_parser: /Users/meganly/rust_projects/json_parser/src/main.rs
+/Users/meganly/rust_projects/json_parser/target/debug/json_parser: /Users/meganly/rust_projects/json_parser/src/main.rs /Users/meganly/rust_projects/json_parser/src/parser.rs
